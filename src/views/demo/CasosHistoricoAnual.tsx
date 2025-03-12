c852ab3e054de9da563ef44f6c84c2bc6eb12a9f
Marta Quiroga Cuervo <martaquirogacuervo@MacBook-Pro-de-Marta.local>
2025-03-12 11:09:42 +0100

    filter by name of group done

--- src/views/demo/CasosHistoricoAnual.tsx
+++ src/views/demo/CasosHistoricoAnual.tsx
@@ -37,23 +37,15 @@ const obtenerFechaEnEspañol = (fecha: string) => {
         'Noviembre',
         'Diciembre',
     ]
-
-    // Crear una nueva fecha con el formato 'YYYY-MM'
-    const fechaObj = new Date(fecha + '-01') // Añadimos el día 1 para convertirlo en un objeto Date válido
-
-    // Obtener el mes y el año
-    const mes = meses[fechaObj.getMonth()] // getMonth() devuelve el índice del mes (0-11)
-    const año = fechaObj.getFullYear() // Obtener el año
-
-    return `${mes} ${año}` // Devolver el resultado en formato 'Mes Año'
+    const fechaObj = new Date(fecha + '-01')
+    const mes = meses[fechaObj.getMonth()]
+    const año = fechaObj.getFullYear()
+    return `${mes} ${año}`
 }
 
 const convertirFechaAFormato = (fecha: Date) => {
-    // Obtener el año y el mes
     const año = fecha.getFullYear()
-    const mes = (fecha.getMonth() + 1).toString().padStart(2, '0') // getMonth() devuelve un índice de 0 a 11, por lo que sumamos 1
-
-    // Formatear como 'YYYY-MM'
+    const mes = (fecha.getMonth() + 1).toString().padStart(2, '0')
     return `${año}-${mes}`
 }
 
@@ -61,7 +53,9 @@ const CasosHistoricoAnual = () => {
     const [chartData, setChartData] = useState<ChartData | null>(null)
     const [tipoUrgencia, setTipoUrgencia] = useState<string>('')
     const [tiposUrgencia, setTiposUrgencia] = useState<string[]>([])
-    const [items, setItems] = useState<DataItem[]>([]) // Datos originales
+    const [grupo, setGrupo] = useState<string>('')
+    const [grupos, setGrupos] = useState<string[]>([])
+    const [items, setItems] = useState<DataItem[]>([])
     const [startDate, setStartDate] = useState(
         () => new Date(new Date().setMonth(new Date().getMonth() - 3)),
     )
@@ -77,22 +71,24 @@ const CasosHistoricoAnual = () => {
         fetch('/data/casos_dashboard.json')
             .then((response) => response.json())
             .then((data: DataItem[] | { data: DataItem[] }) => {
-                //parsedData es parea almacenar la estructura que tenga la variable Data
                 const parsedData: DataItem[] = Array.isArray(data)
                     ? data
                     : data.data
-
                 if (!parsedData || parsedData.length === 0) {
                     console.error('No hay datos disponibles')
                     return
                 }
                 setItems(parsedData)
-
-                //aqui estamos cogiendo los dos tipos de valores que tiene tipo_urgencia y guardandolos en el estado de TiposUrgencia
-                const uniqueUrgencias = Array.from(
-                    new Set(parsedData.map((item) => item.tipo_urgencia)),
+                setTiposUrgencia(
+                    Array.from(
+                        new Set(parsedData.map((item) => item.tipo_urgencia)),
+                    ),
                 )
-                setTiposUrgencia(uniqueUrgencias)
+                setGrupos(
+                    Array.from(
+                        new Set(parsedData.map((item) => item.nombre_grupo)),
+                    ),
+                )
             })
             .catch((error) =>
                 console.error('Error al cargar los datos JSON:', error),
@@ -101,14 +97,14 @@ const CasosHistoricoAnual = () => {
 
     useEffect(() => {
         if (items.length === 0) return
-        // aqui utilizo el metodo reduce para reducir los casos a un solo valor y meterlo en el objeto grouped
         const grouped = items.reduce<{
-            [key: string]: { total: number; filtro: number } // aqui decimos que mes_anio, su clave es string
+            [key: string]: { total: number; filtro: number }
         }>((acc, item) => {
-            const casos = parseInt(item.total_casos, 10) || 0 // aqui, como total_casos es un string en el json, lo pasamos a numero
+            if (grupo && item.nombre_grupo !== grupo) return acc
+            const casos = parseInt(item.total_casos, 10) || 0
             const key = item.mes_anio
             acc[key] = acc[key] || { total: 0, filtro: 0 }
-            acc[key].total += casos //suma de los casos totales
+            acc[key].total += casos
             if (!tipoUrgencia || item.tipo_urgencia === tipoUrgencia) {
                 acc[key].filtro += casos
             }
@@ -116,27 +112,10 @@ const CasosHistoricoAnual = () => {
         }, {})
 
         const fechasOrdenadas = Object.keys(grouped).reverse()
-
         const data = fechasOrdenadas.filter((item) => {
             const fechaInicial = convertirFechaAFormato(startDate)
             const fechaFinal = convertirFechaAFormato(endDate)
-
-            if (fechaInicial && fechaFinal) {
-                return (
-                    item.toString() >= fechaInicial &&
-                    item.toString() <= fechaFinal
-                )
-            }
-
-            if (fechaInicial) {
-                return item.toString() >= fechaInicial
-            }
-
-            if (fechaFinal) {
-                return item.toString() >= fechaFinal
-            }
-
-            return item
+            return item >= fechaInicial && item <= fechaFinal
         })
 
         setChartData({
@@ -156,7 +135,7 @@ const CasosHistoricoAnual = () => {
                     zoom: { enabled: false },
                     events: { click: handleClick },
                 },
-                colors: [COLOR_1, '#D3D3D3'], // Filtrados en color principal, total en gris
+                colors: [COLOR_1, '#D3D3D3'],
                 fill: {
                     type: 'gradient',
                     gradient: {
@@ -172,36 +151,51 @@ const CasosHistoricoAnual = () => {
                     categories: data.map(obtenerFechaEnEspañol),
                     type: 'category',
                 },
-                yaxis: {
-                    opposite: true,
-                    max: 500, // Fijo el máximo d y en 500
-                },
+                yaxis: { opposite: true, max: 500 },
                 legend: { horizontalAlign: 'left' },
             },
         })
-    }, [tipoUrgencia, items, handleClick, startDate, endDate])
+    }, [tipoUrgencia, grupo, items, handleClick, startDate, endDate])
 
-    if (!chartData) {
-        return <div>Loading...</div>
-    }
+    if (!chartData) return <div>Loading...</div>
 
     return (
         <div>
             <h2 className="text-lg font-bold mb-4">Casos Históricos Anual</h2>
-            <div className="mb-4">
-                <label className="mr-2">Filtrar por tipo de urgencia:</label>
-                <select
-                    className="p-2 border rounded"
-                    value={tipoUrgencia}
-                    onChange={(e) => setTipoUrgencia(e.target.value)}
-                >
-                    <option value="">Todos</option>
-                    {tiposUrgencia.map((urgencia) => (
-                        <option key={urgencia} value={urgencia}>
-                            {urgencia}
-                        </option>
-                    ))}
-                </select>
+            <div className="mb-4 flex gap-4">
+                <div>
+                    {' '}
+                    <label className="mr-2">Filtrar por grupo:</label>
+                    <select
+                        className="p-2 border rounded"
+                        value={grupo}
+                        onChange={(e) => setGrupo(e.target.value)}
+                    >
+                        <option value="">Todos</option>
+                        {grupos.map((grp) => (
+                            <option key={grp} value={grp}>
+                                {grp}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div>
+                    <label className="mr-2">
+                        Filtrar por tipo de urgencia:
+                    </label>
+                    <select
+                        className="p-2 border rounded"
+                        value={tipoUrgencia}
+                        onChange={(e) => setTipoUrgencia(e.target.value)}
+                    >
+                        <option value="">Todos</option>
+                        {tiposUrgencia.map((urgencia) => (
+                            <option key={urgencia} value={urgencia}>
+                                {urgencia}
+                            </option>
+                        ))}
+                    </select>
+                </div>
                 <DatePickerComponent
                     startDate={startDate}
                     endDate={endDate}
